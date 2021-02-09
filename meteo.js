a422a6f85e9b4d19d5cf47c553f2118598a7d22a
wassimdouiri <78760084+wassimdouiri@users.noreply.github.com>
2021-02-09 22:22:13 +0100

    Modulariser

---
+++ meteo.js
@@ -0,0 +1,27 @@
+var Paris = document.getElementById("Parisbox");
+var Guinéé = document.getElementById("Guinéébox");
+var Tunisie = document.getElementById("Tunisiebox");
+
+function showCity(){
+
+
+var element = document.getElementById("City").value;
+
+if ( element=="Tunisie"){
+Paris.style.visibility = "hidden";
+    Tunisie.style.visibility = "visible";
+    Guinéé.style.visibility = "hidden";
+
+}
+
+else if ( element=="Guinéé"){
+Paris.style.visibility = "hidden";
+    Tunisie.style.visibility = "hidden";
+    Guinéé.style.visibility = "visible";
+}
+else if  ( element=="Paris"){
+Paris.style.visibility = "visible";
+    Tunisie.style.visibility = "hidden";
+    Guinéé.style.visibility = "hidden";
+}
+}
